cc6d03142745c7e36a7303ba0f26148b6defa01a
Rhyojaan <Chad_Hughes8877@yahoo.com>
2026-06-28 23:52:47 -0400

    Add script loader for 5.4.1

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+(function(){
+  const files = [
 
+    "js/app-part-01.js"
+
+  ];
+
+  function loadNext(index){
+
+    if(index >= files.length) return;
+
+    const s = document.createElement("script");
+
+    s.src = files[index];
+
+    s.onload = () => loadNext(index + 1);
+
+    s.onerror = () => alert("Aldren could not load " + files[index] + ". Please check that the file exists in GitHub.");
+
+    document.body.appendChild(s);
+
+  }
+
+  loadNext(0);
+
+})();
